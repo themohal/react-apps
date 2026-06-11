a2ced1ce46c8c418d41c5139d6d7c840055226ba
Muhammad Farjad Ali Raza <farjadmohal@gmail.com>
2026-06-11 18:57:40 +0500

    class_component

---
+++ class_component/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ClassComponent from './ClassComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <ClassComponent />
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ class_component/src/ClassComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ClassComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, I am a Class Component!</h1>
+                <p>This is a simple example of a class-based component in React.</p>
+            </div>
+        );
+    }
+}
+
+
+export default ClassComponent;

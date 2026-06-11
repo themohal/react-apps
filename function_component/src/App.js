6055b286871f147e7a04057f7584aa027450aa20
Muhammad Farjad Ali Raza <farjadmohal@gmail.com>
2026-06-11 18:21:21 +0500

    functional_component

---
+++ function_component/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FuncComponent from './FuncFile';
+function App() {
+  return (
+    <div className="App">
+      <FuncComponent />
+
+    </div>
+  );
+}
+
+export default App;
